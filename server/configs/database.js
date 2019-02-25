ff75fc87cf0e62b8ad1bf44c2568504289d4164c
snntaylan <sinan.taylan@outlook.com>
2019-02-25 12:24:21 +0100

    mern app

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv')
 
 // Don't forget to set "MONGODB_URI" in ~/server/.env
 const uri = process.env.MONGODB_URI || `mongodb://localhost/please-set-process-env-mongodb-uri`;
 
 mongoose
-  .connect(uri, { useNewUrlParser: true })
+  .connect("mongodb://localhost/react-countries", { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
